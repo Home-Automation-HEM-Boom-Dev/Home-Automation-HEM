fcdd7526d441e83e3ef61019fd877a782946c5ef
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-11-26 20:32:24 +0200

    feat: Created User component and added images

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Login from "../src/components/login/Login.jsx"
+import User from "../src/components/user/User.jsx"
 
 export default function Index() {
-  return <Login />;
+  return <User username={"John Doe"} avatar={'/images/avatar.png'} size={70}/>;
 }
