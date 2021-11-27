a9de30d1bd9053a07cf406c444c8ce29ea65f452
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-11-27 15:58:34 +0200

    fixing and centering login form and applying classes

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,18 +1,20 @@
-import LoginForm from "../../src/components/login/LoginForm";
+import Login from "../../src/components/login/Login";
 import classNames from "classnames";
 import { Container, Paper } from "@mui/material";
 import styles from "../../src/components/login/LoginPage.module.scss"
 
 
-export default function LoginPage(){
+export default function LoginPage() {
 
     return (
-     <div className={classNames(styles['wrapper'])}>
-      <Paper>
-          <Container>
-              <LoginForm />
-          </Container>
-      </Paper>
-     </div>
+        <div className={classNames(styles['wrapper'])}>
+
+            <Container style={{ height: "100vh", display: "flex", alignItems: "center"}} >
+                <Paper style={{ width: "fit-content", margin: "0 auto"}} >
+                    <Login />
+                </Paper>
+            </Container>
+
+        </div>
     )
 }
