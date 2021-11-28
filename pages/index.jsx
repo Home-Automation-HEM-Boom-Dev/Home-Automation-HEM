9ec58ecdd2d1ea0076c397abb712d5378d58e458
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-11-28 10:35:34 +0200

    creating time component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import Login from "../src/components/login/Login.jsx"
 import User from "../src/components/user/User.jsx"
 import Weather from "../src/components/weather/Weather.jsx"
+import Time from "../src/components/time/Time.jsx";
 
 export default function Index() {
   
 return (
   <>
     <Weather degrees={22} type={"cloudy"}/>
+    <Time />
   </>
 );
 }
---
+++ src/components/time/Time.jsx
@@ -0,0 +1,40 @@
+import classNames from "classnames";
+import Typography from '@mui/material/Typography';
+import styles from "./Time.module.scss";
+import { useEffect, useState } from "react";
+
+
+
+
+export default function Time() {
+
+    const [hours, setHours] = useState(new Date().getHours());
+    const [minutes, setMinutes] = useState(new Date().getMinutes());
+
+
+    console.log(hours)
+    console.log(minutes)
+    useEffect(() => {
+
+        let time = setInterval(() => {
+            setHours(new Date().getHours());
+            setMinutes(new Date().getMinutes());
+        }, 1000)
+        return function cleanup() {
+            clearInterval(time)
+        }
+    })
+
+
+
+
+
+    return (
+        <div className={classNames(styles['time-wrapper'])}>
+            <Typography variant="body1" className={classNames(styles['time'])}>Time</Typography>
+            <Typography variant="h1">
+                {hours < 10 ? "0" + hours : hours}:{minutes < 10 ? "0" + minutes : minutes}
+            </Typography>
+        </div>
+    )
+}
