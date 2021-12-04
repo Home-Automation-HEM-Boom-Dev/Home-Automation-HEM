1ef674a42412fbf88258714c67439e3cad55bb9e
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-04 23:12:26 +0200

    feat: Implemented Navigation component and fixed style on components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import User from "../src/components/user/User.jsx"
 import Weather from "../src/components/weather/Weather.jsx"
 import Time from "../src/components/time/Time.jsx";
+import Navigation from "../src/components/navigation/Navigation.jsx"
 import Header from "../src/components/header/Header.jsx";
 
 
@@ -8,13 +9,14 @@ export default function Index() {
 
   return (
     <>
-      <Header left={<User name={"John Doe"} avatar={"/images/avatar.png"} size={90}/>}
+      <Header left={<User name={"John Doe"} avatar={"/images/avatar.png"} size={114} headingSize={"h1"} hasWelcome={true}/>}
         right={<>
           <Weather degrees={22} type={"cloudy"} />
           <Time />
           </>
         }
       />
+      <Navigation />
     </>
   );
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -9,9 +9,48 @@ import { useState } from 'react';
 
 
 export default function Navigation() {
+    const [anchorEl, setAnchorEl] = useState(null);
+   
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div>
-            
-        </div>
-    )
+        <Drawer variant="permanent" className={classNames(styles['drawer'])}>
+            <div className={classNames(styles['user-log-out'])}>
+                <User avatar="images/avatar.png" name="John Doe" size={55} headingSize='h5' />
+                <IconButton onClick={handleClick}>
+                    <MoreVertIcon />
+                </IconButton>
+                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                    <MenuItem className={classNames(styles['log-out'])} onClick={handleClose}>{"Log out"}</MenuItem>
+                </Menu>
+            </div>
+                <List>
+                    {['Apartment', 'Living Room', 'Bedroom', 'Bathroom', 'Toilet', 'Patio'].map((text, index) => (
+                        <ListItem button key={text} className={classNames(index == 0 ? styles.selected : "", styles.listItem)}>
+                            <ListItemIcon>
+                                {index == 0 ? <HomeOutlinedIcon /> : <BedOutlinedIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                            {index == 0 ? <Badge color="secondary" badgeContent={5} /> : null}
+                        </ListItem>
+                    ))}
+                </List>
+                <List>
+                    {['House', 'Living Room', 'Bedroom 1', 'Bedroom 2', 'Bedroom 3', 'Bathroom', 'Toilet', 'Patio'].map((text, index) => (
+                        <ListItem button key={text} className={classNames(index == 0 ? styles.selected : "", styles.listItem)}>
+                            <ListItemIcon>
+                                {index == 0 ? <HomeOutlinedIcon /> : <BedOutlinedIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                            {index == 0 ? <Badge color="secondary" badgeContent={7} /> : null}
+                        </ListItem>
+                    ))}
+                </List>
+        </Drawer>
+    );
 }
