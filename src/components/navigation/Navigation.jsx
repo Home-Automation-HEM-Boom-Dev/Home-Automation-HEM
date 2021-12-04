112363122a4bae0faa592b41b29b60d140045c51
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-04 17:55:05 +0200

    feat: Started working on Navigation component

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { Drawer, List, IconButton, Menu, ListItem, MenuItem, ListItemIcon, Badge, ListItemText } from '@mui/material';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import BedOutlinedIcon from '@mui/icons-material/BedOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import User from '../user/User';
+import classNames from 'classnames';
+import styles from './Navigation.module.scss';
+import { useState } from 'react';
+
+
+export default function Navigation() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
