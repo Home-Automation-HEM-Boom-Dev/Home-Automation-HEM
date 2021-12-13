7b37bf44c019354eb4f79ef762b80c90d7df265a
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-13 22:43:55 +0200

    adding hidden prop when iconUrl is not defined

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,16 +2,17 @@ import styles from "./Card.module.scss";
 import { Card as CardWrap, CardContent, CardMedia, IconButton, Typography} from '@mui/material';
 import classNames from "classnames";
 
-export default function Card({ iconUrl, outlined = false, title}) {
+export default function Card({ iconUrl, outlined = false, title, variant, onClick}) {
     let index = `${iconUrl.length}`;
     let isSvgorVideo = +index - 4;
     const svgOrMp4 = `${iconUrl.substring(isSvgorVideo)}`;
 
     return (
-        <CardWrap className={svgOrMp4 != ".svg" ? classNames(styles['video-card']) : classNames(styles['card'])} style={{ border: outlined ? "3px solid #7441F3" : "" }}>
+        <CardWrap className={classNames(styles['card'])} style={{ border: outlined ? "3px solid #7441F3" : "" }}>
             <CardContent className={svgOrMp4 != ".svg" ? classNames(styles['video-card-content']) : ""}>
                 <IconButton>
-                    {iconUrl && <CardMedia component={svgOrMp4 === ".svg" ? "img" : "video"} src={`${iconUrl}`} autoPlay />}
+                    {iconUrl ? <CardMedia component={svgOrMp4 === ".svg" ? "img" : "video"} src={`${iconUrl}`} autoPlay /> :
+                    <CardMedia hidden={true} component={svgOrMp4 === ".svg" ? "img" : "video"} src={`${iconUrl}`} autoPlay />}
                 </IconButton>
             </CardContent>
             {title ? <Typography className={styles["card-title"]}>{title}</Typography> : null}
--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,11 +1,17 @@
 import classNames from "classnames";
 import styles from "./Devidec.module.scss";
+import Card from "./../../components/card/Card";
+import { Grid, Chip } from "@mui/material";
 
-export default function Devices() {
-
+export default function Devices({devices}) {
+const devicesList = devices.map((device, i) => {
+    <Grid item><Card key={i} title={device.title} iconUrl={device.iconUrl}/></Grid>
+})
     return (
         <div className={classNames(styles['devices-wrapper'])}>
-
+         <Grid container>
+          {devicesList}
+         </Grid>
         </div>
     )
 }
