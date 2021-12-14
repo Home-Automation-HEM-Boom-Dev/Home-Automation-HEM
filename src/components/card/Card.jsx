0d4ea162e4d882a664799ddaba0188c1f34fa7bf
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-14 21:09:14 +0200

    fix: Returned card content

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,14 @@
 import styles from "./Card.module.scss";
-import { Card as CardWrap, Typography } from '@mui/material';
+import { Card as CardWrap, CardContent, Typography } from '@mui/material';
 import classNames from "classnames";
 
 export default function Card({ iconUrl, outlined = false, title, variant, autoPlay, onClick }) {
     return (
         <div className={classNames(styles['card'])} onClick={onClick}>
             <CardWrap className={classNames(styles['card-component'])} style={{ border: outlined ? "4px solid #7545EC" : null }}>
-                {iconUrl ? (iconUrl.includes("svg") ? <img src={iconUrl} alt="img" /> : <video src={iconUrl} autoPlay={autoPlay} alt="video" />) : null}
+                <CardContent>
+                    {iconUrl ? (iconUrl.includes("svg") ? <img src={iconUrl} alt="img" /> : <video src={iconUrl} autoPlay={autoPlay} alt="video" />) : null}
+                </CardContent>
                 {title ? <Typography className={styles["card-title"]}>{title}</Typography> : null}
             </CardWrap>
         </div>
