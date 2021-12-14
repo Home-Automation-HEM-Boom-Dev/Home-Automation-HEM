55a1c04a931cf80f880d03be763baed3a653ba47
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-14 14:50:16 +0200

    fix: Trying to access lenght when iconUrl is undefiened

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,15 @@
 import styles from "./Card.module.scss";
-import { Card as CardWrap, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card as CardWrap, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
 import classNames from "classnames";
 
 export default function Card({ iconUrl, outlined = false, title, variant, onClick }) {
-    let index = `${iconUrl.length}`;
-    let isSvgorVideo = +index - 4;
-    const svgOrMp4 = `${iconUrl.substring(isSvgorVideo)}`;
-
     return (
         <div className={classNames(styles['card'])} onClick={onClick}>
             <CardWrap className={classNames(styles['card-component'])} style={{ border: outlined ? "3px solid #7545EC" : null }}>
                 <CardContent>
-                    {iconUrl ? <CardMedia component={svgOrMp4 === ".svg" ? "img" : "video"} src={`${iconUrl}`} autoPlay /> : null}
+                    <IconButton>
+                        {iconUrl ? <CardMedia component={iconUrl.includes("svg") ? "img" : "video"} src={`${iconUrl}`} autoPlay /> : null}
+                    </IconButton>
                 </CardContent>
                 {title ? <Typography className={styles["card-title"]}>{title}</Typography> : null}
             </CardWrap>
