5878c68e24cad31bed8cdefa2e35ced0ab0a2e2a
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-18 10:01:50 +0200

    removing the chip from devices comp and adding a title to the card

--- src/components/devices/Devices.jsx
+++ src/components/devices/Devices.jsx
@@ -1,10 +1,28 @@
 import classNames from "classnames";
 import styles from "./Devices.module.scss";
 import Card from "./../../components/card/Card";
-import { Grid, Chip } from "@mui/material";
+import { Grid } from "@mui/material";
+
+// const cards = [
+//     {
+//         title: "on",
+//         iconUrl: "../../../public/images/plug.svg"
+//     },
+//     {
+//         title: "off",
+//         iconUrl: "../../../public/images/bulb.svg"
+//     },
+//     {
+//         title: "offline",
+//         iconUrl: "../../../public/images/plug.svg"
+//     },
+//     {
+//         title: "off",
+//         iconUrl: "../../../public/images/plus.svg"
+//     },
+// ]
 
 export default function Devices({ devices }) {
-
 
     return (
         <div className={classNames(styles['devices-wrapper'])}>
@@ -12,7 +30,7 @@ export default function Devices({ devices }) {
                 {devices.map((dev, i) => {
                     return (
                         <Grid item xs={12} sm={6} lg={3} key={i}><Card  title={dev.title} iconUrl={dev.iconUrl} /></Grid>
-                )
+                        )
                 })}
             </Grid>
         </div>
