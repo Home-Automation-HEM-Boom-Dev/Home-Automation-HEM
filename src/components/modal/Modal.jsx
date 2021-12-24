05b47c3c3c3d162384b67b320ec4bb4ff2767f40
Nikola Hadzhiev <nikihad2012@abv.bg>
2021-12-24 19:42:02 +0200

    fix: Not rendering children

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,11 +2,11 @@ import styles from "./Modal.module.scss";
 import classNames from "classnames";
 import { Dialog, DialogTitle, DialogContent, Button } from "@mui/material";
 
-export default function Modal({ open = true, title = "Modal title", buttonText = "Modal Action", handleSubmit, handleClose }) {
+export default function Modal({ open = true, title = "Modal title", buttonText = "Modal Action", children, handleSubmit, handleClose }) {
     return (
         <Dialog open={open} onClose={handleClose} className={classNames(styles["modal-wrapper"])} >
             <DialogTitle>{title}</DialogTitle>
-            <DialogContent>Modal Content</DialogContent>
+            <DialogContent>{children}</DialogContent>
             <Button onClick={handleSubmit}>{buttonText}</Button>
         </Dialog>
     )
