f0f56b5136bc7f016bdff244eafad5a402c410b9
Nikola Hadzhiev <nikihad2012@abv.bg>
2022-01-11 21:43:34 +0200

    fix: CircularProgress

--- src/components/devices/AddDevice.jsx
+++ src/components/devices/AddDevice.jsx
@@ -3,14 +3,14 @@ import classNames from "classnames";
 import styles from './AddDevice.module.scss';
 import Modal from "../modal/Modal";
 
-export default function AddDevice({ open, searching, found, failed, onDevice, handleSubmit, handleClose }) {
+export default function AddDevice({ open, searching = true, found, failed, onDevice, handleSubmit, handleClose }) {
     return (
         <Modal open={open} handleSubmit={handleSubmit} handleClose={handleClose} title={"ADD DEVICE"}
             buttonProps={{ "color": "primary", "display": found || failed ? "inline-block" : "none" }}
             buttonText={found ? "ADD DEVICE" : 'TRY AGAIN'}>
 
             <div className={classNames(styles.container)}>
-                <CircularProgress size={394} sx={{ opacity: 0.3 }} disableShrink variant={found || failed ? "determinate" : searching ? "indeterminate" : ""} value={0} />
+                <CircularProgress size={394} sx={{ opacity: 0.3 }} variant={failed ? "determinate" : searching ? "indeterminate" : ""} value={0} />
                 <CircularProgress size={394}
                     sx={{ opacity: failed ? 0.15 : found && 0.4, position: "absolute", color: found ? "primary" : failed && "rgba(24, 28, 36, 1)" }}
                     variant={found || failed ? "determinate" : "indeterminate"}
