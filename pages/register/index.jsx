e13ecf549a5a86419f5a24c3a053fab5511b7879
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2022-01-15 00:05:25 +0200

    added paragraph for redirecting to login

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -5,12 +5,20 @@ import { Container, Paper } from "@mui/material";
 
 export default function RegisterPage() {
     return (
+       
         <div className={classNames(styles['wrapper'])}>
             <Container style={{ height: "100vh", display: "flex", alignItems: "center"}} >
                 <Paper style={{ width: "fit-content", margin: "0 auto"}} >
                     <Register />
+                    
                 </Paper>
+                
             </Container>
+            <div className={classNames(styles['login-redirect'])} >
+                <p>Already registered?<a href="/login">Go to login.</a></p> 
+            </div>  
         </div>
+        
+        
     )
 }
